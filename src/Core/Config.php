4d2a15bc95e65bc1bff21d1c579fcc4e13d71f4e
xpromx <xpromx@gmail.com>
2018-04-24 18:17:05 +0900

    config/helpers.php

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -2,46 +2,38 @@
 
 namespace Travelience\Aida\Core;
 
-class Config {
-
-    public $config = ['app', 'mail', 'seo' ,'services', 'database'];
+class Config
+{
+    public $config = ['app', 'mail', 'seo' ,'services', 'database', 'helpers'];
 
     public function handle($req, $res)
     {
                 
         // config defaults
-        foreach( $this->config as $file )
-        {
+        foreach ($this->config as $file) {
             $path = FRAMEWORK_PATH . '/config/' . $file . '.php';
-            $this->load($file,  $path);
+            $this->load($file, $path);
         }
 
         // configs merge
-        foreach( $this->config  as $file )
-        {   
+        foreach ($this->config  as $file) {
             $path = CONFIG_PATH . '/' . $file . '.php';
-            $this->load($file,  $path);
+            $this->load($file, $path);
         }
-
     }
 
     public function load($key, $path)
     {
-        if( !file_exists( $path ) )
-        {
+        if (!file_exists($path)) {
             return false;
         }
 
         $config = include $path;
 
-        if( isset( $GLOBALS['config'][ $key ] ) )
-        {
+        if (isset($GLOBALS['config'][ $key ])) {
             return $GLOBALS['config'][ $key ] = array_merge($GLOBALS['config'][ $key ], $config);
         }
 
         return $GLOBALS['config'][ $key ] = $config;
-        
     }
-
-
 }
