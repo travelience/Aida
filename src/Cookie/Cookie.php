abc34f5258d548c2dc3f382585d4cdbb85e82d28
xpromx <xpromx@gmail.com>
2018-06-13 15:06:33 +0900

    Cookie force update

--- src/Cookie/Cookie.php
+++ src/Cookie/Cookie.php
@@ -2,56 +2,52 @@
 
 namespace Travelience\Aida\Cookie;
 
-class Cookie {
-    
+class Cookie
+{
     public static function flush()
     {
-        if( count($_COOKIE) )
-        {
-            foreach( $_COOKIE as $key=>$value )
-            {
+        if (count($_COOKIE)) {
+            foreach ($_COOKIE as $key=>$value) {
                 Cookie::forget($key);
             }
         }
     }
 
-    public static function forget( $key )
+    public static function forget($key)
     {
         setcookie($key, null, -1, '/');
     }
 
-    public static function get( $key, $default=false )
+    public static function get($key, $default=false)
     {
-        if( isset($_COOKIE[$key]) )
-        {
+        if (isset($_COOKIE[$key])) {
             return unserialize($_COOKIE[$key]);
         }
 
         return $default;
     }
 
-    public static function has( $key )
+    public static function has($key)
     {
-        if( isset($_COOKIE[$key]) )
-        {
+        if (isset($_COOKIE[$key])) {
             return true;
         }
 
         return false;
     }
 
-    public static function put( $key, $value, $duration=60 )
+    public static function put($key, $value, $duration=60)
     {
-        if( is_array($value) ||  is_object($value) )
-        {
+        if (is_array($value) ||  is_object($value)) {
             $value = serialize($value);
         }
 
         setcookie($key, $value, time() + $duration, '/');
+        $_COOKIE[$key] = $value;
         return $value;
     }
 
-    public static function set( $key, $value, $duration=60 )
+    public static function set($key, $value, $duration=60)
     {
         self::put($key, $value, $duration);
     }
